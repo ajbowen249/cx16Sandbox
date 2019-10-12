91499d79dc69a60cfc4cc657fb156b495eb64dde
Alex Bowen <ajbowen249@gmail.com>
2019-10-12 13:34:47 -0400

    sprites work

--- src/hello.asm
+++ src/hello.asm
@@ -1,6 +1,6 @@
-    * = $0801 ; RUN command jumps here
+* = $0801 ; RUN command jumps here
 
-    .include "vera.asm"
+.include "vera.asm"
 
 init          ; No idea what this is doing. Got it from the Facebook group.
     asl $0a08 ; Nothing works without it.
@@ -12,10 +12,9 @@ init          ; No idea what this is doing. Got it from the Facebook group.
     brk
     brk
 
-    * = $0810
+* = $0810
 
 main
-
     lda #$05 ; green on black
     jsr clear_screen_80_60
 
@@ -35,10 +34,76 @@ loop
 
 done
 
+    ; Increment 1 per data write, start at the first sprite register
+    #set_vera_addr #$1F, #$50, #$00
+
+    ; Our sprite will be store in vram, starting at address 10000
+    lda #$00
+    sta VERA_DATA0 ; Address 12:5
+    lda #$08
+    sta VERA_DATA0 ; Mode 0, some junk, and address 16:13
+
+    lda #$ff ; X 255
+    sta VERA_DATA0
+    lda #$0 ; Zero for X second byte
+    sta VERA_DATA0
+
+    lda #$ff ; Y 255
+    sta VERA_DATA0
+    lda #$0 ; Zero for Y second byte
+    sta VERA_DATA0
+
+    lda #$0C ; blank collision mask, z-depth 3, zero flip
+    sta VERA_DATA0
+
+    lda #$00 ; 8x8, palette offset 0
+    sta VERA_DATA0
+
+    ; We said the sprite started at 0, so write something there
+    #set_vera_addr #$11, #$00, #$00
+    lda #$ff
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+    sta VERA_DATA0
+
+    ; Enable sprite 0
+    #set_vera_addr #$1F, #$40, #$00
+    lda #$01
+    sta VERA_DATA0
+
 wait
     bra wait ; block forever
     rts ; done - return to basic (if we ever got here)
 
 mytext .text 'HELLO', $6c, $60, 'WORLD', $61, 0
 
-    .include "subroutines.asm"
+.include "subroutines.asm"
--- src/vera.asm
+++ src/vera.asm
@@ -1,15 +1,15 @@
-VERA_ADDR_LO = $9f20
+VERA_ADDR_HI = $9f20
 VERA_ADDR_MID = $9f21
-VERA_ADDR_HI = $9f22
+VERA_ADDR_LO = $9f22
 
 VERA_DATA0 = $9f23
 VERA_DATA1 = $9f24
 
-set_vera_addr .macro low, mid, high
+set_vera_addr .macro high, mid, low
     lda \low
-    sta VERA_ADDR_LO
+    sta VERA_ADDR_HI
     lda \mid
     sta VERA_ADDR_MID
     lda \high
-    sta VERA_ADDR_HI
+    sta VERA_ADDR_LO
     .endm
